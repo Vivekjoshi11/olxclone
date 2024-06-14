cf52d16452d1e503a78b2a1b797691dc10db93d8
Vivekjoshi11 <108278744+Vivekjoshi11@users.noreply.github.com>
2024-06-14 16:56:51 +0530

    all ui done

--- src/app/chat-safety-tips/chat-safety-tips.component.ts
+++ src/app/chat-safety-tips/chat-safety-tips.component.ts
@@ -7,4 +7,33 @@ import { Component } from '@angular/core';
 })
 export class ChatSafetyTipsComponent {
 
+  alertdelet(): void {
+    let txt: string;
+    if (confirm("Sure You want to delete Account?")) {
+        txt = "You pressed OK!";
+    } else {
+        txt = "You pressed Cancel!";
+    }
+    const demoElement = document.getElementById("demo");
+    if (demoElement) {
+        demoElement.innerHTML = txt;
+    } else {
+        console.error("Element with id 'demo' not found.");
+    }
+}
+
+logout(): void {
+  let txt: string;
+  if (confirm("Sure You want to logout?")) {
+      txt = "You pressed OK!";
+  } else {
+      txt = "You pressed Cancel!";
+  }
+  const demoElement = document.getElementById("demo");
+  if (demoElement) {
+      demoElement.innerHTML = txt;
+  } else {
+      console.error("Element with id 'demo' not found.");
+  }
+}
 }
